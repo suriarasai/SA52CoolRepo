4c6f464de69bcd7b2000559edc4d14a7a9818080
Suria R Asai <suria.r.asai@gmail.com>
2021-06-24 14:48:34 +0800

    Started CA project

---
+++ alex/src/main/java/sg/edu/iss/alex/AlexApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.iss.alex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexApplication.class, args);
+	}
+
+}
---
+++ alex/src/test/java/sg/edu/iss/alex/AlexApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.iss.alex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
